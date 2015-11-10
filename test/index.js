c1dfa40b5085bd93c97ca78a4f4d40185c9e42fd
lemangarM <mohamed.lemangar@gmail.com>
2015-11-10 16:06:13 +0100

    update tests

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 /*eslint-disable*/
 var expect = require('chai').expect;
 var is = require('../lib/is/index');
-Array.isArray = null;
+//Array.isArray = null;
 
 describe('is micro-checking library', function() {
   describe('is.arguments', function () {
@@ -23,13 +23,59 @@ describe('is micro-checking library', function() {
       expect(is.array(['value1', 'value2'])).to.be.true;
     });
     it('should return false if passed argument type is not Array', function () {
-      expect(is.array('yassine')).to.be.false;
+      expect(is.array('mohamed')).to.be.false;
     });
   });
-
   describe('is.whoWillBeLigue1Champion', function () {
     it('should return PSG in uppercase', function() {
       expect(is.whoWillBeLigue1Champion()).to.be.equal('PSG');
     });
   });
+ describe('is.boolean', function(){
+   it('should return true if is boolean', function(){
+    // var bool = new boolean();
+     expect(is.boolean(false)).to.be.true;
+     expect(is.boolean(true)).to.be.true;
+   });
+   it('should return false if not boolean', function(){
+     expect(is.boolean('notboolean')).to.be.false;
+   });
+ });
+ describe('is.date', function(){
+   it('should return true if is date', function(){
+     var date = new Date();
+     expect(is.date(date)).to.be.true;
+   });
+   it('should return false if not date', function(){
+     expect(is.date('notDate')).to.be.false;
+   });
+ });
+ describe('is.error', function(){
+   it('should return true if is error', function(){
+     var erreur = new Error();
+     expect(is.error(erreur)).to.be.true;
+   });
+   it('should return false if not error', function(){
+     expect(is.error('notError')).to.be.false;
+   });
+ });
+ describe('is.function', function(){
+   it('should return true if is function', function(){
+     var f = function(){
+     };
+     expect(is.function((f))).to.be.true;
+   });
+   it('should return false if not function', function(){
+     expect(is.function('notFunction')).to.be.false;
+   });
+ });
+ describe('is.nan', function(){
+   it('should return true if is nan', function(){
+     expect(is.nan(NaN)).to.be.true;
+   });
+   it('should return false if not nan', function(){
+     expect(is.nan('jugj')).to.be.false;
+   });
+ });
+
 });
